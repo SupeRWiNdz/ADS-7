313ce655846da9655c6b1b18fecc27a81dbf3ed5
SupeRWiNdz <125024771+SupeRWiNdz@users.noreply.github.com>
2023-04-24 19:48:10 +0300

    Update tpqueue.h

--- include/tpqueue.h
+++ include/tpqueue.h
@@ -1,6 +1,7 @@
 // Copyright 2022 NNTU-CS
 #ifndef INCLUDE_TPQUEUE_H_
 #define INCLUDE_TPQUEUE_H_
+#include <string>
 
 template<typename T>
 class TPQueue {
@@ -19,13 +20,13 @@ class TPQueue {
       Node *head = new Node;
       head->x = x;
     } else {
-      Item* current = head;
+      Node* current = head;
       while (current->x.prior <= current->next->x.prior && current != nullptr) {
         current = current->next;
     }
-      Item* temp = new Node;
-      temp->next= current->next;
-      current->next= temp;
+      Node* temp = new Node;
+      temp->next = current->next;
+      current->next = temp;
       temp->x = x;
   return;
   }
